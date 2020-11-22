5e7134047d5f6bbfb26a8580be2cfc0ef1a20095
Vitalii Yarmus <55307415+325Vitalik@users.noreply.github.com>
2020-11-22 12:08:38 +0200

    Make user birthday nullable

---
+++ Goder.DAL/Migrations/20201122100447_MakeBirthdayOfUserNullable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Goder.DAL.Migrations
+{
+    public partial class MakeBirthdayOfUserNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Birthday",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Birthday",
+                table: "Users",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+        }
+    }
+}
