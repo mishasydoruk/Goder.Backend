e3affd4825418c5282496abfbdac6e9e55147783
Vitalii Yarmus <55307415+325Vitalik@users.noreply.github.com>
2020-12-19 19:18:01 +0200

    Fix SolutionDTO

--- Goder.BL/DTO/SolutionCreateDTO.cs
+++ Goder.BL/DTO/SolutionCreateDTO.cs
@@ -5,7 +5,7 @@ namespace Goder.BL.DTO
     public class SolutionCreateDTO
     {
         public string Script { get; set; }
-
+        public Guid CreatorId { get; set; }
         public Guid ProblemId { get; set; }
     }
 }
